7b26debd3319de07ee5a1941488c20746a84c179
Indunil1992 <indunil@adroitlogic.com>
2019-10-28 15:34:01 +0530

    delete the S3 buc frm editr

--- testGitIndu28/lambda.js
+++ testGitIndu28/lambda.js
@@ -1,26 +1,5 @@
-let AWS = require('aws-sdk');
-const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
-    s3.copyObject({
-        'Bucket': "sigma.smoketest",
-        'CopySource': `/as2-test-lahiru/10`,
-        'Key': "10"
-    }).promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                CopyObjectResult: {
-                    ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                    LastModified: "<Date Representation>"
-                }
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
-
+ 
     callback(null, { "message": "Successfully executed" });
 }

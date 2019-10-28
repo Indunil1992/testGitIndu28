c259a0d373f910796add4c89d39595f995e5579d
Indunil1992 <indunil@adroitlogic.com>
2019-10-28 15:27:42 +0530

    Add S3 Buc with Copy OP

--- testGitIndu28/lambda.js
+++ testGitIndu28/lambda.js
@@ -1,4 +1,26 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = function (event, context, callback) {
+    s3.copyObject({
+        'Bucket': "indunil.trigger",
+        'CopySource': `/test.indunil1/1`,
+        'Key': "1"
+    }).promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                CopyObjectResult: {
+                    ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+                    LastModified: "<Date Representation>"
+                }
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+
+    callback(null, { "message": "Successfully executed" });
 }
